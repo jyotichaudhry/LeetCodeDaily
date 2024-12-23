5415e22d0ec0bb4e24aa73620b514a71612ce2bc
Jyoti Chaudhary <61777545+jyotichaudhry@users.noreply.github.com>
2024-12-23 18:42:19 +0530

    Time: 5 ms (92.08%) | Memory: 45.9 MB (33.46%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,40 +1,33 @@
-class CustomStack {
+
 
  
-    Stack<Integer> stack1 = new Stack<>();
-    Stack<Integer> stack2 = new Stack<>();
-   int maxStackSize ;
-
+    class CustomStack {
+	int[] stack;
+    int size;
+    int index;
     public CustomStack(int maxSize) {
-        this.maxStackSize = maxSize;
+        stack = new int[maxSize];
+        size = maxSize;
+        index=0;
     }
 
     public void push(int x) {
-        if (stack1.size() < maxStackSize){
-            stack1.push(x);
+       if (index<size){
+            stack[index++] = x;
         }
     }
 
     public int pop() {
-return stack1.isEmpty() ?  -1:  stack1.pop();
+        if(index == 0) return -1;
+        return stack[--index];
     }
 
     public void increment(int k, int val) {
-        if (stack1.isEmpty()) return;
-        int diff = stack1.size() - k;
-        while (!stack1.isEmpty() ){
-            if (diff > 0){
-                stack2.add(stack1.pop());
-            } else {
-                stack2.add(stack1.pop()+val);
-            }
-             diff--;
-        }
-        while (!stack2.isEmpty()){
-            stack1.add(stack2.pop());
-        }
+        if(stack.length == 0) return;
+        for(int i = 0;i<k && i<stack.length;i++)
+            stack[i] = stack[i] + val;
     }
-}
+    }
 
 /**
  * Your CustomStack object will be instantiated and called as such:
